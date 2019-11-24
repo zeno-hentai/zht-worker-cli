0c37c6b0748d16e4c0786f0155873a12bb121cdb
Jaccob Kii <jaccobkii@gmail.com>
2019-11-25 03:44:59 +1100

    bgufix

--- src/crawlers/e-hentai.ts
+++ src/crawlers/e-hentai.ts
@@ -11,12 +11,56 @@ async function test(url: string): Promise<boolean> {
     return !!url.match(/http?s:\/\/e-hentai\.org\/g\/\d+\/[A-Za-z]+\d+\//)
 }
 
+function parseMetaPage(doc: CheerioStatic): [Exclude<GalleryMeta, 'pageNumber' | 'files'>, string[], string] {
+    const title = doc("#gn").text()
+    const jpTitle = doc("#gj").text()
+    return [
+        {
+            title,
+            subTitles: {
+                'jp': jpTitle
+            }
+        },
+        [],
+        ""
+    ]
+}
+
+function parseImagePage(doc: CheerioStatic): [string, string | null] {
+
+}
+
 async function download(url: string, proxy: CrawlerProxyConfig | null, client: CrawlerMetaClient<GalleryMeta>): Promise<boolean> {
     const agent = axios.create({
         proxy: proxy || undefined
     });
     const doc = cheerio.load((await agent.get(url)).data)
-    doc(".")
+    const [meta, tags, firstUrl] = parseMetaPage(doc)
+    const images: [string, ArrayBuffer][] = []
+    let pageUrl: string | null = firstUrl
+    while(pageUrl != null){
+        const imgDoc = cheerio.load((await agent.get(pageUrl)).data)
+        const [imgUrl, nextPage] = parseImagePage(imgDoc)
+        const ext = imgUrl.match(/\.(\w+)$/)[1]
+        const {data} = (await agent.get(imgUrl))
+        images.push([ext, data])
+        pageUrl = nextPage
+    }
+    const files: {[key: string]: string} = {}
+    images.forEach(([ext, _], idx) => {
+        files[idx] = `${idx}.${ext}`
+    })
+    const {uploadFile} = await client.uploadMeta({
+        ...meta,
+        pageNumber: images.length,
+        files
+    }, tags)
+    let index = 0
+    for(let [ext, imageData] of images) {
+        const name = `${index++}.${ext}`
+        await uploadFile(name, imageData)
+    }
+    return true
 }
 
 export const EHentaiCrawler: ZHTCrawler<GalleryMeta> = {
